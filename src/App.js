5fe2cb0a77dfb732ea2f331a7b9ce33b1c4ad0a7
K3nFischer <kendalljfischer@gmail.com>
2021-10-24 20:49:13 -0700

    added API_Interface.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import MainPage from "./MainPage";
+import TopBar from "./menu/TopBar";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import {CssBaseline} from "@mui/material";
+
+const customTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: "#1c6a87"
+    }
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        text: {
+          color: "#ffffff"
+        }
+      }
+    }
+  }
+});
+
+
 
 function App() {
   return (
-      <MainPage/>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+          <TopBar/>
+      </ThemeProvider>
   );
 }
 
